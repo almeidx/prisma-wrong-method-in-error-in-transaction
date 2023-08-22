69f438d0ce80a6f6b3e606f5d8e5cc8ae8803855
Almeida <almeidx@pm.me>
2023-08-22 23:42:26 +0100

    add code

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to run
+
+1. `pnpm install`
+1. `pnpm build`
+1. `pnpm start`
+
+## Result
+
+(Likely) The most interesting logs:
+
+![result](./result.png)
+
+The full log is [here](./log.txt).
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["error", "info", "query", "warn"],
+});
+
+await prisma.$transaction([
+  prisma.model1.deleteMany({}),
+  prisma.model2.create({
+    data: {
+      field2: "value",
+      // @ts-expect-error: Showcase
+      faultyField: "value",
+    },
+  }),
+]);
